6471848ff138bf2694b7bf2a54353e39dcb703af
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-24 16:11:28 +0100

    crud books

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,12 @@ exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var mongoose_1 = __importDefault(require("mongoose"));
 var book_model_1 = __importDefault(require("./models/book.model"));
+var body_parser_1 = __importDefault(require("body-parser"));
 var app = (0, express_1["default"])();
+// app.use(bodyParser.json)
+app.use(body_parser_1["default"].json());
 var uri = 'mongodb://root:AMSHXbKIoiG2@192.168.64.4:27017/biblio?authSource=admin';
-var connection = mongoose_1["default"].connect(uri, function (err) {
+mongoose_1["default"].connect(uri, function (err) {
     if (err) {
         console.log(err.message);
     }
@@ -26,9 +29,68 @@ app.get('/books', function (req, res) {
         }
     });
 });
+// ?page=Xsize=Y
+app.get('/booksParPage', function (req, res) {
+    var _a, _b;
+    var page = parseInt(((_a = req.query.page) === null || _a === void 0 ? void 0 : _a.toString()) || '1');
+    var size = parseInt(((_b = req.query.size) === null || _b === void 0 ? void 0 : _b.toString()) || '5');
+    book_model_1["default"].paginate({}, { page: page, limit: size }, function (err, books) {
+        if (err)
+            res.status(500).send(err);
+        else
+            res.send(books);
+    });
+});
+app.get('/booksSearch', function (req, res) {
+    var _a, _b;
+    var search = req.query.search || '';
+    var page = parseInt(((_a = req.query.page) === null || _a === void 0 ? void 0 : _a.toString()) || '1');
+    var size = parseInt(((_b = req.query.size) === null || _b === void 0 ? void 0 : _b.toString()) || '5');
+    book_model_1["default"].paginate({ title: { $regex: ".*(?i)" + search + ".*" } }, { page: page, limit: size }, function (err, books) {
+        if (err)
+            res.status(500).send(err);
+        else
+            res.send(books);
+    });
+});
+app.post('/books', function (req, res) {
+    var book = new book_model_1["default"](req.body);
+    book.save(function (err) {
+        if (err)
+            return res.status(500).send(err);
+        else
+            return res.status(200).send(book);
+    });
+});
+app["delete"]('/books/:id', function (req, res) {
+    book_model_1["default"].findByIdAndDelete(req.params.id, function (err) {
+        if (err)
+            return res.status(500).send(err);
+        else
+            return res.send("book deleted");
+    });
+});
+app.put('/books/:id', function (req, res) {
+    book_model_1["default"].findByIdAndUpdate(req.params.id, req.body, function (err, book) {
+        if (err)
+            return res.status(500).send(err);
+        else
+            return res.status(200).send(book);
+    });
+});
+app.get('/books/:id', function (req, res) {
+    book_model_1["default"].findById(req.params.id, function (err, book) {
+        if (err)
+            return res.status(500).send(err);
+        else
+            return res.status(200).send(book);
+    });
+});
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-app.listen(3000, function () {
-    console.log('Server is running on port 3000');
+var port = 8085;
+var connected = false;
+app.listen(port, function () {
+    console.log('Server is running on port ' + port);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express,{Request,Response} from 'express';
 import mongoose from 'mongoose';
 import Book from './models/book.model';
+import bodyParser from 'body-parser';
 const app = express();
-
+// app.use(bodyParser.json)
+app.use(bodyParser.json());
 const uri = 'mongodb://root:AMSHXbKIoiG2@192.168.64.4:27017/biblio?authSource=admin';
-const connection = mongoose.connect(uri,(err) => {
+mongoose.connect(uri,(err) => {
 	if (err) {
 		console.log(err.message);
 	} else {
@@ -22,10 +24,72 @@ app.get('/books',(req:Request,res:Response)=>{
 	});
 });
 
+//pagination
+app.get('/booksParPage',(req:Request,res:Response)=>{
+	const page:number = parseInt(req.query.page?.toString()||'1');
+	const size:number = parseInt(req.query.size?.toString()||'5');
+
+	Book.paginate({},{page:page,limit:size},(err:any,books:any)=>{
+		if(err) res.status(500).send(err);
+		else res.send(books);
+	});
+})
+
+// search and paginate
+app.get('/booksSearch',(req:Request,res:Response)=>{
+	const search = req.query.search || '';
+	const page:number = parseInt(req.query.page?.toString()||'1');
+	const size:number = parseInt(req.query.size?.toString()||'5');
+
+	Book.paginate({title:{$regex:".*(?i)"+search+".*"}},{page:page,limit:size},(err:any,books:any)=>{
+		if(err) res.status(500).send(err);
+		else res.send(books);
+	});
+	
+});
+
+// create
+app.post('/books',  (req:Request,res:Response) => {
+	const book = new Book(req.body);
+	book.save((err)=>{
+		if(err) return res.status(500).send(err);
+		else return res.status(200).send(book);
+	})
+
+});
+
+//delete
+app.delete('/books/:id',(req:Request,res:Response) => {
+	Book.findByIdAndDelete(req.params.id,(err: any)=>{
+		if(err) return res.status(500).send(err);
+		else return res.send("book deleted");
+	})
+});
+
+// update
+app.put('/books/:id', (req:Request,res:Response) => {
+	Book.findByIdAndUpdate(req.params.id,req.body,(err:any,book:any)=>{
+		if(err) return res.status(500).send(err);
+		else return res.status(200).send(book);
+	})
+});
+
+// find by id
+app.get('/books/:id',(req:Request,res:Response) => {
+	Book.findById(req.params.id,(err:Error,book:any)=>{
+		if(err) return res.status(500).send(err);
+		else return res.status(200).send(book);
+	})
+});
+
+
+
+
 app.get('/', (req, res) => {
 	  res.send('Hello World!');
 });
-
-app.listen(3000, () => {
-	  console.log('Server is running on port 3000');
+let port = 8085;
+let connected = false;
+app.listen(port, () => {
+	console.log('Server is running on port '+port);
 });
